d537e4e28d5b639fa5442c26f6ce8c0bc21df4c7
Mohamed Eltokhy <eltokeymohamed444@email.com>
2026-08-05 12:14:33 +0300

    feat(di): register UploadApiService in RegisterModule

--- lib/core/di/register_module.dart
+++ lib/core/di/register_module.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:network_leyer/core/network/retrofit/upload_api_service.dart';
 import 'package:network_leyer/features/auth/data/datasources/auth_api_client.dart';
 
 import '../network/api_endpoints.dart';
@@ -13,23 +14,29 @@ abstract class RegisterModule {
 
   @lazySingleton
   Dio dio(ConnectivityInterceptor connectivityInterceptor) {
-    final dio = Dio(
-      BaseOptions(
-        baseUrl: ApiEndpoints.baseUrl,
-        connectTimeout: const Duration(seconds: 15),
-        receiveTimeout: const Duration(seconds: 15),
-        contentType: Headers.jsonContentType,
-        headers: {
-          Headers.acceptHeader: Headers.jsonContentType,
-          "X-SECRET-KEY": ApiEndpoints.secretKey,
-        },
-      ),
-    )
-      ..interceptors.add(connectivityInterceptor)
-      ..interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+    final dio =
+        Dio(
+            BaseOptions(
+              baseUrl: ApiEndpoints.baseUrl,
+              connectTimeout: const Duration(seconds: 15),
+              receiveTimeout: const Duration(seconds: 15),
+              contentType: Headers.jsonContentType,
+              headers: {
+                Headers.acceptHeader: Headers.jsonContentType,
+                "X-SECRET-KEY": ApiEndpoints.secretKey,
+              },
+            ),
+          )
+          ..interceptors.add(connectivityInterceptor)
+          ..interceptors.add(
+            LogInterceptor(requestBody: true, responseBody: true),
+          );
     return dio;
   }
 
   @lazySingleton
   AuthApiClient authApiClient(Dio dio) => AuthApiClient(dio);
+
+  @lazySingleton
+  UploadApiService uploadApiService(Dio dio) => UploadApiService(dio);
 }
